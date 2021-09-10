5bbb1faa0247af4f225519160659b4f13559f03b
ZenwayProjects <zenway15@gmail.com>
2021-09-10 18:02:23 -0500

    creacion de la carpeta back

---
+++ factoria_back/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+require("dotenv").config();
+
+//routes
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Funciona" });
+});
+
+//port
+const port = process.env.PORT;
+
+//listen.port
+app.listen(port, () => {
+  console.log(`Aplicacion de MYSQL corriendo ${port}`);
+});
