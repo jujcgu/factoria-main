7bd5a6e03173ed11935d99500c7e850cd824bec9
jujcgu <juanjosecastrogutierrez@gmail.com>
2021-09-13 14:56:47 -0500

    conectores_pais_servidor

--- factoria_servidor/index.js
+++ factoria_servidor/index.js
@@ -62,6 +62,52 @@ app.delete("/api/delete-roles/:id", (req, res) => {
   });
 });
 
+app.get('/api/select-pais', (req, res) => {
+
+  const sqlRolesSelect = "SELECT * FROM factoria.pais";
+  connection.query(sqlRolesSelect, (err, result) => {
+    res.send(result);        
+  });
+});
+
+app.post('/api/insert-pais', (req, res) => {
+const id = req.body.id;
+const nombre = req.body.nombre;
+const moneda = req.body.moneda;
+
+const sqlRolesInsert = "INSERT INTO pais (id, nombre, moneda) VALUES(?, ?, ?)";
+connection.query(sqlRolesInsert, [id, nombre, moneda], (err, result) => {
+  console.log(result);
+});
+});
+
+app.put("/api/update-pais", (req, res) => {
+const id = req.body.id;
+const nombre = req.body.nombre;
+connection.query(
+  "UPDATE pais SET nombre = ?, moneda = ? WHERE id = ?",
+  [nombre, moneda, id],
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  }
+);
+});
+
+app.delete("/api/delete-pais/:id", (req, res) => {
+const id = req.params.id;
+connection.query("DELETE FROM pais WHERE id = ?", id, (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    res.send(result);
+  }
+});
+});
+
 app.listen(port, () => {
   console.log(`El servidor esta corriendo en http://localhost:${port}`)
 })
